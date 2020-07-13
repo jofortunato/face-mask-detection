f94aa337aa630483485a1272153c627e0e200684
Joao Fortunato <joaomlfortunato@gmail.com>
2020-07-13 22:17:31 +0100

    Personalize Files for Masks Model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1o3oJEbnpScZZa2feFfpO5xYewpfqPoHb?alt=media&key=AIzaSyAdFORqeYEQlYNbhCoEAhks7vetacQ-Aow'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['face-mask', 'face-shield-with-mask', 'face-shield-without-mask', 'incorrect-face-mask-usage', 'no-mask']
 path = Path(__file__).parent
 
 app = Starlette()
